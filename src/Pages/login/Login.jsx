0d5bb49ae9e2cf3c54c80811b2fef924f7b00b46
Rymney34 <104559353+Rymney34@users.noreply.github.com>
2026-04-30 17:24:54 +0100

    test3

--- src/Pages/login/Login.jsx
+++ src/Pages/login/Login.jsx
@@ -40,7 +40,7 @@ console.log("API URL:", import.meta.env.VITE_API_URL);
 
     const handleSubmit = async (values, { setSubmitting, setStatus }) => {
         try {
-          const response = await fetch(`${API}${API_ENDPOINT}`, {
+          const response = await fetch(`${API_ENDPOINT}`, {
               method: "POST",
               headers: { "Content-Type": "application/json" },
               body: JSON.stringify(values),
